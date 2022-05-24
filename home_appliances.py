23e284b98190dc73e1467d26591658a479314d09
Yulia Shvets <yuliia-mariia.shvets.ir.2021@lpnu.ua>
2022-05-24 11:55:27 +0300

    Fixed home_appliances.py

--- home_appliances.py
+++ home_appliances.py
@@ -1,4 +1,5 @@
 class HomeAppliances:
     def __init__(self, producer: str, name: str, price_in_uah: float):
         self.producer = producer
+        self.name = name
         self.price = price_in_uah
