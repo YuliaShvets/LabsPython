9c2a9b5d82882e43727194eed8c283cf85902db6
Yulia Shvets <yuliia-mariia.shvets.ir.2021@lpnu.ua>
2022-06-10 10:48:19 +0300

    Modified file regex.py

--- README.md
+++
@@ -1,3 +0,0 @@
-Task for the first Python lab :
-- Implement heirarchy of classes.
-- Derivated class must contain additional fields.
--- regex.py
+++ regex.py
@@ -1,18 +1,17 @@
 import re
-from datetime import datetime
 
-pattern = r'GET /images/stands/TH_photo_[0-9]+\.jpg HTTP/1\.1'
 
-file_name = "access.txt"
-date = '%d/%b/%Y:%H:%M:%S'
-date_from = datetime.strptime('24/Mar/2009:14:39:35', date)
-date_to = datetime.strptime('25/Mar/2009:11:43:29', date)
-file = open(file_name, 'r')
-lines = file.readlines()
+def main():
+    file = open("access.txt", "r")
+    lines = file.readlines()
+    pattern = re.compile(r"GET /images/stands/TH_photo_[0-9]+\\.jpg HTTP/1\\.1\" 200")
 
-count = 0
-for line in lines:
-    count += 1
-    split_line = re.split(pattern, line)
+    count = 0
+    for line in lines:
+        count += 1
+        matches = pattern.match(line)
+    print(count)
 
-print(count)
+
+if __name__ == '__main__':
+    main()
